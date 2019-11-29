f27cbc78319510125460675b9adcd17214ab0291
jason.guo <ghlwyyx@sina.com>
2019-11-29 08:52:14 +0800

    promise判断

---
+++ src/isPromise.ts
@@ -0,0 +1,8 @@
+import { isDef } from "./isDef"
+export function isPromise(val: any): boolean {
+    return (
+        isDef(val) &&
+        typeof val.then === 'function' &&
+        typeof val.catch === 'function'
+    )
+}
